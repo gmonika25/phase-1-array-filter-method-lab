e094cae168dab2dfb740c743a8b7539b66b6c5ad
Monika Gustilo <monikagustilo@gmail.com>
2022-07-09 17:23:12 -0700

    commit tree 4385c5db940a4de32689a2d68adfdd447f0bf392

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+function findMatching(arr, driverName) {
+  let newArr = [];
+  for (const element of arr) {
+    element.toLowerCase();
+    newArr.push(element);
+  }
+  let result = newArr.filter(
+    (n) => n.toLowerCase() === driverName.toLowerCase()
+  );
+  return result;
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(arr, letters) {
+  // let newArr = [];
+  // for (const element of arr) {
+  //   element.toLowerCase();
+  //   newArr.push(element);
+  // }
+  // letters = letters.toLowerCase();
+  let fuzzyResult = arr.filter((driver) => driver.startsWith(letters));
+  return fuzzyResult;
+}
+
+console.log(fuzzyMatch(drivers, "Sa"));
+
+const drivers2 = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(arr, str) {
+  let matchResults = arr.filter((s) => s.name === str);
+  return matchResults;
+}
+
+console.log(matchName(drivers2, "Bobby"));
